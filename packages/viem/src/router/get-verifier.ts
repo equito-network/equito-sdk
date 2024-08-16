4d3e305db9be41976b06f28975de0bbcad1d9bdc
Matteo Muraca <mmuraca247@gmail.com>
2024-08-16 11:50:52 +0200

    viem: fix `getVerifier` return type

--- packages/viem/src/router/get-verifier.ts
+++ packages/viem/src/router/get-verifier.ts
@@ -20,21 +20,16 @@ export type GetVerifierArgs = {
  * Gets the verifier address.
  *
  * @param {GetVerifierArgs} args {@link GetVerifierArgs}
- * @returns {Promise<EquitoAddress>} The verifier address.
+ * @returns {Promise<Hex>} The verifier address.
  */
 export const getVerifier = async ({
   publicClient,
   routerContract,
-}: GetVerifierArgs): Promise<EquitoAddress> => {
-  const verifier = await publicClient.readContract({
+}: GetVerifierArgs): Promise<Hex> => {
+  return await publicClient.readContract({
     address: routerContract,
     abi: routerAbi,
     functionName: "verifiers",
     args: [BigInt(0)],
   });
-
-  return {
-    lower: encodeAbiParameters(parseAbiParameters("address"), [verifier]),
-    upper: encodeAbiParameters(parseAbiParameters("bytes1"), ["0x00"]),
-  };
 };
