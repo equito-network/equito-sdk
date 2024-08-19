0b6d9849b0e7de07722ac1b2b5dde96430cb5f59
Matteo Muraca <56828990+muraca@users.noreply.github.com>
2024-08-19 14:32:31 +0200

    doc: `generateHash` functions (#28)

--- packages/ethers/src/utils/index.ts
+++ packages/ethers/src/utils/index.ts
@@ -2,19 +2,17 @@ import { EquitoMessage, Hex } from "@equito-sdk/core";
 import { AbiCoder, keccak256 } from "ethers";
 
 /**
- * @title generateHash
- * @notice Generates a Keccak256 hash of an EquitoMessage object.
- * @dev This function encodes the EquitoMessage structure in `EquitoMessage` specific format and then hashes it.
- * 
- * @param {EquitoMessage} message - The message object to be hashed.
- * @returns {Hex} The Keccak256 hash of the encoded message, represented as a hexadecimal string.
+ * Generates the Keccak256 hash of an EquitoMessage object.
+ *
+ * @param message {@link EquitoMessage} - The message object to be hashed.
+ * @returns The Keccak256 hash of the encoded message, represented as a hexadecimal string.
  */
 export const generateHash = (message: EquitoMessage): Hex =>
   keccak256(
     AbiCoder.defaultAbiCoder().encode(
       [
-        "tuple(uint256 blockNumber,uint256 sourceChainSelector,tuple(bytes32 lower,bytes32 upper) sender,uint256 destinationChainSelector,tuple(bytes32 lower,bytes32 upper) receiver,bytes32 hashedData)"
+        "tuple(uint256 blockNumber,uint256 sourceChainSelector,tuple(bytes32 lower,bytes32 upper) sender,uint256 destinationChainSelector,tuple(bytes32 lower,bytes32 upper) receiver,bytes32 hashedData)",
       ],
-      [message],
-    ),
+      [message]
+    )
   ) as Hex;
