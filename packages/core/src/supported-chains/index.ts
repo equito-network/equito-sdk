e1fa200211d39de9d95c85d44206f15412f0ef58
Matteo Muraca <mmuraca247@gmail.com>
2024-08-19 14:21:38 +0200

    core/supported-chains

--- packages/core/src/supported-chains/index.ts
+++ packages/core/src/supported-chains/index.ts
@@ -3,41 +3,41 @@ import { EquitoChain } from "../types";
 
 // Assert correct type for chain-selectors.json file
 const chainSelectors = chainSelectorsJson as Record<
-    string,
-    { names: string[]; }
+  string,
+  { names: string[] }
 >;
+
 /**
- * @title getChainSelectorByName
- * @description Returns the chain selector value for a given chain name.
- * @param {string} name - The name of the chain to look up.
- * @returns {number} - The chain selector associated with the given chain name.
- * @throws {Error} - Throws an error if the provided chain name is not supported.
- * 
+ * Returns the chain selector value for a given chain name.
+ * @param {string} name The name of the chain to look up.
+ * @returns The chain selector associated with the given chain name.
+ * @throws Throws an error if the provided chain name is not supported.
+ *
  * @example
+ * ```
  * const selector = getChainSelectorByName("ethereum");
  * console.log(selector); // Outputs: 1
+ * ```
  */
 export function getChainSelectorByName(name: string): number {
-    const lowerCaseName = name.toLowerCase();
+  const lowerCaseName = name.toLowerCase();
 
-    for (const [selector, { names }] of Object.entries(chainSelectors)) {
-        if (names.includes(lowerCaseName)) {
-            return Number(selector);
-        }
+  for (const [selector, { names }] of Object.entries(chainSelectors)) {
+    if (names.includes(lowerCaseName)) {
+      return Number(selector);
     }
+  }
 
-    throw new Error(`Invalid or unsupported Chain Name: ${name}`);
+  throw new Error(`Invalid or unsupported Chain Name: ${name}`);
 }
 
-
 /**
- * @title getAllSupportedChains
- * @description Retrieves all supported chains with their respective chain selector, name, and image path.
- * 
- * @returns {EquitoChain []}
- * - An array of objects, each containing the chain selector, names, and image path for a supported chain.
- * 
+ * A list of all supported chains.
+ *
+ * @returns An array of objects, each containing the chain selector and names for a supported chain.
+ *
  * @example
+ * ```
  * const chains = getAllSupportedChains();
  * console.log(chains);
  * // [
@@ -45,30 +45,33 @@ export function getChainSelectorByName(name: string): number {
  * //   { chainSelector: 2, names: ["bsc", "bnb"], image: "images/bsc.svg" },
  * //   ...
  * // ]
+ * ```
  */
 export function getAllSupportedChains(): EquitoChain[] {
-    const supportedChains = [];
-    for (const [selector, { names }] of Object.entries(chainSelectors)) {
-        supportedChains.push({
-            chainSelector: Number(selector),
-            names,
-        });
-    }
-    return supportedChains;
+  const supportedChains = [];
+  for (const [selector, { names }] of Object.entries(chainSelectors)) {
+    supportedChains.push({
+      chainSelector: Number(selector),
+      names,
+    });
+  }
+  return supportedChains;
 }
 
 /**
- * @description Retrieves the chain information, including names or aliases and image, associated with a specific chain selector.
- * @param {number} chainSelector - The selector value for the chain.
- * @returns {EquitoChain} - An object containing the chain selector, names, and image associated with the chain.
- * @throws {Error} - Throws an error if the chain selector is invalid or not found.
+ * Returns the chain information, including names or aliases, associated with a specific chain selector.
+ * 
+ * @param {number} chainSelector The selector value for the chain.
+ * @returns An object containing the chain selector, names, and image associated with the chain.
+ * 
+ * @throws Throws an error if the chain selector is invalid or not found.
  */
 export function getByChainSelector(chainSelector: number): EquitoChain {
-    const chainData = chainSelectors[chainSelector];
+  const chainData = chainSelectors[chainSelector];
 
-    if (chainData) {
-        return { chainSelector, ...chainData };
-    }
+  if (chainData) {
+    return { chainSelector, ...chainData };
+  }
 
-    throw new Error(`Invalid or unsupported Chain Selector: ${chainSelector}`);
+  throw new Error(`Invalid or unsupported Chain Selector: ${chainSelector}`);
 }
