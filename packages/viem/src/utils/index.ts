828bb3150652c7a27aafd11e6b812afaaad1cd24
Matteo Muraca <mmuraca247@gmail.com>
2024-08-19 14:01:52 +0200

    doc: `generateHash` functions

--- packages/viem/src/utils/index.ts
+++ packages/viem/src/utils/index.ts
@@ -1,7 +1,12 @@
 import { EquitoMessage, Hex } from "@equito-sdk/core";
 import { encodeAbiParameters, keccak256, parseAbiParameters } from "viem";
 
-
+/**
+ * Generates the Keccak256 hash of an EquitoMessage object.
+ *
+ * @param message {@link EquitoMessage} - The message object to be hashed.
+ * @returns The Keccak256 hash of the encoded message, represented as a hexadecimal string.
+ */
 export const generateHash = (message: EquitoMessage): Hex =>
   keccak256(
     encodeAbiParameters(
@@ -20,4 +25,3 @@ export const generateHash = (message: EquitoMessage): Hex =>
       ]
     )
   );
-
