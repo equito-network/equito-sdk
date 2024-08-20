9aaa5188e141e2ec9b277a45c8f234b770d35c21
K Gunjan <gunjan.cn@gmail.com>
2024-08-20 14:23:19 +0530

    chore(all): prettier formatting (#30)

--- packages/core/src/types/misc.ts
+++ packages/core/src/types/misc.ts
@@ -3,15 +3,14 @@ export type Hex = `0x${string}`;
 /**
  * @interface EquitoChain
  * @description Represents a blockchain configuration used within the Equito ecosystem.
- * 
- * @property {number} chainSelector - A unique numeric identifier for the blockchain. 
+ *
+ * @property {number} chainSelector - A unique numeric identifier for the blockchain.
  * Typically used to differentiate between different blockchains or network configurations.
- * 
- * @property {string[]} names - An array of strings representing different names or aliases 
+ *
+ * @property {string[]} names - An array of strings representing different names or aliases
  * for the blockchain. This can include common names, abbreviations, or other recognized identifiers.
  */
 export interface EquitoChain {
-    chainSelector: number;
-    names: string[];
+  chainSelector: number;
+  names: string[];
 }
-
--- packages/ethers/src/router/get-fee.ts
+++ packages/ethers/src/router/get-fee.ts
@@ -6,18 +6,18 @@ import { AbstractProvider, Contract } from "ethers";
  * The arguments for the getFee function.
  */
 export type GetFeeArgs = {
-    /**
-     * The {@link AbstractProvider} that will be used to read the contract.
-     */
-    provider: AbstractProvider;
-    /**
-     * The router contract in {@link Hex} format.
-     */
-    routerContract: Hex;
-    /**
-     * The sender contract in {@link Hex} format.
-     */
-    senderContract: Hex;
+  /**
+   * The {@link AbstractProvider} that will be used to read the contract.
+   */
+  provider: AbstractProvider;
+  /**
+   * The router contract in {@link Hex} format.
+   */
+  routerContract: Hex;
+  /**
+   * The sender contract in {@link Hex} format.
+   */
+  senderContract: Hex;
 };
 
 /**
@@ -27,18 +27,18 @@ export type GetFeeArgs = {
  * @returns {Promise<bigint>} The fee in wei for a given sender contract.
  */
 export const getFee = async ({
-    provider,
-    routerContract,
-    senderContract,
+  provider,
+  routerContract,
+  senderContract,
 }: GetFeeArgs): Promise<bigint> => {
-    const contract = new Contract(routerContract, routerAbi, provider);
-    const getFeeMethod = contract.getFee as (arg: Hex) => Promise<bigint>;
+  const contract = new Contract(routerContract, routerAbi, provider);
+  const getFeeMethod = contract.getFee as (arg: Hex) => Promise<bigint>;
 
-    if (!getFeeMethod) {
-        throw new Error("The getFee method is not defined on the contract");
-    }
+  if (!getFeeMethod) {
+    throw new Error("The getFee method is not defined on the contract");
+  }
 
-    const fee: bigint = await getFeeMethod(senderContract);
+  const fee: bigint = await getFeeMethod(senderContract);
 
-    return fee;
+  return fee;
 };
